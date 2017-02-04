7dd744990bde16f2ea59b1caf7f7464102f4a5ec
raymondproduction@ukr.net <raymondproduction@ukr.net>
2017-02-05 01:36:44 +0200

    I use karma and added file .gitignore in repository

---
+++ js/test/testSpec.js
@@ -0,0 +1,13 @@
+define(['map'],function(mapModule){
+
+  describe('Test for map module',function(){
+    var map = new mapModule();
+    it('works test',function(){
+
+
+
+      expect(true).toEqual(true);
+    });
+
+  });
+});
